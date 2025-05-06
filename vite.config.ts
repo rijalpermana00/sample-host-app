1fc0be523a9f21e4213cf8d87e7f5f1b3e1c7514
Rijal Permana <rijalpermana00@gmail.com>
2025-05-06 23:24:56 +0700

    update version:

--- vite.config.ts
+++ vite.config.ts
@@ -10,13 +10,16 @@ export default defineConfig({
         federation({
             name: 'my-host-app',
             remotes: {
-                // "my-remote-app": "http://localhost:5174/assets/remoteEntry.js",
-                "my-remote-app": "https://sample-remote-app.netlify.app/assets/remoteentry.js"
+                "my-remote-app": "http://localhost:5174/assets/remoteEntry.js",
+                // "my-remote-app": "https://sample-remote-app.netlify.app/assets/remoteentry.js"
             },
             shared: ['vue', 'tailwindcss']
         })
     ],
     build:{
         "target": "esnext",  // Changed from ES2020 to ES2022 to support top-level await
+        "minify": false,
+        "cssCodeSplit": false,
+        "cssMinify": false,
     },
 })
