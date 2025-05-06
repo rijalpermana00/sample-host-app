f1be8d139e112530d1734bde537df90a2d712d44
Rijal Permana <rijalpermana00@gmail.com>
2025-05-06 13:40:05 +0700

    feat: add federation plugin to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import federation from "@originjs/vite-plugin-federation";
 
-
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
-        tailwindcss()
+        tailwindcss(),
+        federation({
+            name: 'my-host-app',
+            // remotes: {
+            //     remote_app: "http://localhost:5001/assets/remoteEntry.js",
+            // },
+            shared: ['vue']
+        })
     ],
 })
